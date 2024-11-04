13b8d0fc833b39ff2598467896ae659a9d85f257
lucselmes <lselmes@gmail.com>
2024-11-04 11:44:58 -0500

    Update TAVILY API KEY HANDLING

--- src/scripts/web_search/companyWebSearch.py
+++ src/scripts/web_search/companyWebSearch.py
@@ -1,7 +1,39 @@
 from tavily import TavilyClient
 import os
+import boto3
+import json
 
-TAVILY_API_KEY = os.environ['TAVILY_API_KEY']
+def get_secret():
+
+    secret_name = "TAVILY_API_KEY_3"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except Exception as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    secret = json.loads(secret)['TAVILY_API_KEY_3']
+
+    # Your code goes here.
+    return secret
+
+TAVILY_API_KEY = get_secret()
+# else:W
+#     TAVILY_API_KEY = os.environ['TAVILY_API_KEY']
 
 class companyWebSearch():
 
